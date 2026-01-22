88f70597e6be7f28e4b834f2075214622f656472
Maël Guiraud <mguiraud@cesi.fr>
2026-01-22 17:18:11 +0100

    logo add

--- app.py
+++ app.py
@@ -225,8 +225,8 @@ def create_app() -> Flask:
                 logo_path,
                 width - 10 * mm - 45 * mm,
                 height - 10 * mm - 18 * mm,
-                width=45 * mm,
-                height=18 * mm,
+                width=100 * mm,
+                height=40 * mm,
                 preserveAspectRatio=True,
                 mask="auto",
             )
